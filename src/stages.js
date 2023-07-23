3d469ee1f35b0ba85d32effaa483ee755690c6a6
charlyautomatiza <charlyautomatiza@gmail.com>
2023-07-23 20:28:00 -0300

    feat: first load testing script with k6

---
+++ src/stages.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    stages: [
+        // We need 3 stages
+        { duration: '10s', target: 5 },
+        { duration: '15s', target: 10 },
+        { duration: '10s', target: 0 }
+    ],
+}
+
+export default function () {
+  http.get('https://test.k6.io');
+  sleep(1);
+}
